09b4ba6d7035a7992e8b02d3239ef96f0ba452db
Cromermw <mark.cromer@gmail.com>
2024-11-03 17:02:30 +0700

    Moved label connections

---
+++ scenes/attributes_display.gd
@@ -0,0 +1,16 @@
+class_name AttributesDisplay
+extends Control
+
+## labels for the attributes and their values
+@export var reputationlabel : Label
+@export var reputationvaluelabel : Label
+@export var karmalabel : Label
+@export var karmavaluelabel : Label
+@export var fitnesslabel : Label
+@export var fitnessvaluelabel : Label
+@export var wealthlabel : Label
+@export var wealthvaluelabel : Label
+@export var educationlabel : Label
+@export var educationvaluelabel : Label
+@export var wisdomlabel : Label
+@export var wisdomvaluelabel : Label
--- scripts/main_view.gd
+++ scripts/main_view.gd
@@ -1,20 +1,6 @@
 class_name MainView
 extends Control
 
-## labels for the attributes and their values
-@export var reputationlabel : Label
-@export var reputationvaluelabel : Label
-@export var karmalabel : Label
-@export var karmavaluelabel : Label
-@export var fitnesslabel : Label
-@export var fitnessvaluelabel : Label
-@export var wealthlabel : Label
-@export var wealthvaluelabel : Label
-@export var educationlabel : Label
-@export var educationvaluelabel : Label
-@export var wisdomlabel : Label
-@export var wisdomvaluelabel : Label
-
 ## buttons for each activity and navigation
 @export var livebutton : Button
 @export var socializebutton : Button
