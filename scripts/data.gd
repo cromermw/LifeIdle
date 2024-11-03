4deecaafa5588816fd582a64eb92937af13df9fd
Cromermw <mark.cromer@gmail.com>
2024-11-03 21:50:04 +0700

    Building out the titles tab

--- scripts/data.gd
+++ scripts/data.gd
@@ -2,10 +2,24 @@ class_name Data
 extends Resource
 ## contains data for saving and loading
 
-## current amount of reputation
+## current amount of each attribute
 @export var reputation : float = 0
 @export var karma : float = 0
 @export var fitness : float = 0
 @export var wealth : float = 0
 @export var education : float = 0
 @export var wisdom : float = 0
+
+##unlock status of titles
+@export var acolyte_unlocked : bool = false
+@export var recruit_unlocked : bool = false
+@export var vendor_unlocked : bool = false
+@export var docent_unlocked : bool = false
+@export var postulant_unlocked : bool = false
+
+##title experience
+@export var karma_experience : int = 0
+@export var fitness_experience : int = 0
+@export var wealth_experience : int = 0
+@export var education_experience : int = 0
+@export var wisdom_experience : int = 0
